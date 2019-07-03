7c22057fbc10b7a217447f19204ede94ea52510b
Zain <zshafiqu@ucsc.edu>
2019-07-03 13:16:20 -0700

    mobile code generator done

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -217,4 +217,56 @@
       code += "}\n"
     e("adCode").value = code
   };
-// ------------ END INTERSTITIAL.HTML -----------------------------
+// ------------ END INTERSTITIAL.HTML -------------------------------
+// ------------ BEGIN MOBILECODEGEN.HTML ----------------------------
+  function e(n) {
+    return document.getElementById(n);
+  };
+  function isHTML() {
+    return e("htmlAdType").checked;
+  };
+  function updateType() {
+    if (isHTML()) {
+      e("htmlOptions").style.display = "block";
+      e("imageOptions").style.display = "none";
+    } else {
+      e("htmlOptions").style.display = "none";
+      e("imageOptions").style.display = "block";
+    }
+  };
+  function URL(name) {
+    return e(name).value.replace(/\|/g, "%7C");
+  };
+  function generateMobileCode() {
+    var code = "{\n";
+
+    if (isHTML()) {
+      var html = e("htmlCode").value;
+      html = html.replace(/\\/g, "\\\\");
+      html = html.replace(/"/g, '\\"');
+      html = html.replace(/\n/g, "\\n");
+      html = html.replace(/\r/g, "\\r");
+      html = html.replace(/\t/g, "\\t");
+      code += '  "html":"' + html + '",\n';
+    } else {
+      code += '  "imageUrl":"' + URL("imageURL") + '",\n';
+      if (URL("clickURL") != "") {
+        code += '  "clickUrl":"' + URL("clickURL") + '",\n';
+      }
+    }
+
+    if (e("logonSponsor").value != "") {
+      code += '  "logonSponsor":"' + e("logonSponsor").value + '",\n';
+    }
+
+    code += '  "impressionUrl":"' + URL("impressionURL") + '",\n';
+    code += '  "height":' + e("adHeight").value + '\n';
+
+    code += "}\n"
+    e("adCode").value = code;
+  };
+  // function diffImage(img) {
+  //    if(img.src.match("img/dog.png")) img.src = "img/answer_dog.png";
+  //    else img.src = "img/answer_dog.png";
+  //  };
+// ------------ END MOBILECODEGEN.HTML ------------------------------
